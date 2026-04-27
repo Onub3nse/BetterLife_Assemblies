7408aab5662de2c15ba5f01c0023d6dd9c07a7a7
Onub3nse <43469130+Onub3nse@users.noreply.github.com>
2026-04-27 14:39:02 +0200

    more fixes...
    Added config.json, user can now on start define a new ore sorting plant capacity.

--- BetterLifeExt.cs
+++ BetterLifeExt.cs
@@ -1,17 +1,18 @@
-﻿using Mafi.Base;
+﻿using BetterLife;
+using Mafi;
+using Mafi.Base;
+using Mafi.Collections;
 using Mafi.Core;
+using Mafi.Core.Buildings.OreSorting;
 using Mafi.Core.Entities;
+using Mafi.Core.Entities.Static.Layout;
+using Mafi.Core.Game;
 using Mafi.Core.Mods;
 using Mafi.Core.Population;
+using Mafi.Core.Products;
+using Mafi.Core.Prototypes;
 using Mafi.Serialization;
 using System;
-using Mafi.Core.Entities.Static.Layout;
-using Mafi.Core.Prototypes;
-using BetterLife;
-using Mafi.Collections;
-using Mafi.Core.Game;
-using Mafi.Core.Products;
-using Mafi;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
@@ -89,10 +90,24 @@ namespace BetterLife_Assemblies
         {
             ProtosDb prototypesDb = registrator.PrototypesDb;
             registrator.RegisterAllProducts();
-            registrator.RegisterData<BetterLifesExt>();
             registrator.RegisterData<AssembliesDef>();
             registrator.RegisterData<RecipesDef>();
             registrator.RegisterData<ResearchDt>();
+
+            ProtosDb protosDb = registrator.PrototypesDb;
+
+
+            OreSortingPlantProto myOreSorter =
+                registrator.PrototypesDb.GetOrThrow<OreSortingPlantProto>(Ids.Buildings.OreSortingPlantT1);
+
+
+            Quantity newOreCapacity = new Quantity(this.JsonConfig.GetInt("oresorter_capacity"));
+
+
+
+            ReflectionUtils.SetField(myOreSorter, "InputBufferCapacity", newOreCapacity);
+            ReflectionUtils.SetField(myOreSorter, "OutputBuffersCapacity", newOreCapacity);
+
         }
         public void RegisterDependencies(DependencyResolverBuilder depBuilder, ProtosDb protosDb, bool gameWasLoaded)
         {
